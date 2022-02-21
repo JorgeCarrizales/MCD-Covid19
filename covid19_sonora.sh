3a9601cacee72dd006eb0075cea74864d45b7540
JorgeCarrizales <43796633+JorgeCarrizales@users.noreply.github.com>
2022-02-20 20:01:46 -0700

    Update covid19_sonora.sh

--- covid19_sonora.sh
+++ covid19_sonora.sh
@@ -4,11 +4,10 @@ echo "Descargando los datos . . ."
 curl -L -O curl -L -O https://datosabiertos.salud.gob.mx/gobmx/salud/datos_abiertos/datos_abiertos_covid19.zip
 echo "Descomprimiendo datos. . ."
 unzip datos_abiertos_covid19.zip
-echo "Ingresa el nombre del archivo .csv obtenido al descrimprimir datos_abiertos_covid19.zip: el nombre del archivo csv contiene la palabra COVID19MEXICO"
-ls
-read name_data
+echo "El archivo descomprimido se guardara con el nombre covid_data.csv"
+mv *.csv covid_data.csv
 echo "Tomando solo datos de Sonora."
-csvgrep -c ENTIDAD_RES -r 26 $name_data > COVIDSONORA.csv
+csvgrep -c ENTIDAD_RES -r 26 covid_data.csv > COVIDSONORA.csv
 echo "Tomando solo datos de personas de entre 20 y 39 años"
 csvgrep -c EDAD -r "^[2-3][0-9]$" COVIDSONORA.csv > COVIDSONORA_20_39.csv
 echo "Tomando solo los casos positivos"
